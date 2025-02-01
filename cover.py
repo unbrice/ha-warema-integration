34d0214d4a0a3ac43055155d1406c41980afae46
RodgerDodger <81526219+Rodger13Dodger@users.noreply.github.com>
2025-02-01 17:38:29 +0100

    cover.py

--- cover.py
+++ cover.py
@@ -5,8 +5,7 @@ import voluptuous as vol
 
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.cover import (
-    CoverEntity, DEVICE_CLASS_SHADE, SUPPORT_OPEN, SUPPORT_CLOSE,
-    SUPPORT_SET_POSITION, ATTR_POSITION, PLATFORM_SCHEMA)
+    CoverEntity, CoverDeviceClass, CoverEntityFeature, ATTR_POSITION, PLATFORM_SCHEMA)
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -19,12 +18,10 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_UPDATE_INTERVAL, default=600): cv.positive_int
 })
 
-
 def setup_platform(hass, config, add_devices, discovery_info=None):
     from warema_wms import Shade, WmsController
     shades = Shade.get_all_shades(WmsController(config[CONF_WEBCONTROL_SERVER_ADDR]), time_between_cmds=0.5)
     add_devices(WaremaShade(s, config[CONF_UPDATE_INTERVAL]) for s in shades)
-
 
 class WaremaShade(CoverEntity):
     """Represents a warema shade"""
@@ -37,7 +34,7 @@ class WaremaShade(CoverEntity):
         self.last_position = self.position
         self.is_moving = False
         self.state_last_updated = datetime.now()
-        self.next_state_upate = datetime.now()
+        self.next_state_update = datetime.now()
         '''This is needed because, when a move is triggered by HA, sometimes the next status update
         still reports 'not moving' because shitty warema hasn't caught up with reality yet
         and then the next update is delayed until for update_interval seconds'''
@@ -45,31 +42,31 @@ class WaremaShade(CoverEntity):
         self.update_interval = update_interval
 
     def update(self, force=False):
-        if datetime.now() > self.next_state_upate or self.is_moving\
+        if datetime.now() > self.next_state_update or self.is_moving \
                 or datetime.now() < self.force_update_until or force:
             self.last_position = self.position
             self.position, self.is_moving, self.state_last_updated = \
                 self.shade.get_shade_state(True)
             if self.state_last_updated:
-                self.next_state_upate = \
+                self.next_state_update = \
                     self.state_last_updated \
                     + timedelta(seconds=self.update_interval)
             _LOGGER.debug('Update performed for {}'.format(self.name))
         else:
             _LOGGER.debug('Update skipped for {}. Next update {}'
-                          .format(self.name, self.next_state_upate))
+                          .format(self.name, self.next_state_update))
 
     @property
     def device_class(self):
-        return DEVICE_CLASS_SHADE
+        return CoverDeviceClass.SHADE
 
     @property
     def supported_features(self):
-        return SUPPORT_OPEN|SUPPORT_CLOSE|SUPPORT_SET_POSITION
+        return CoverEntityFeature.OPEN | CoverEntityFeature.CLOSE | CoverEntityFeature.SET_POSITION
 
     @property
     def unique_id(self):
-        return 'warema_shade' + self.name
+        return 'warema_shade_{}'.format(self.name)
 
     @property
     def name(self):
@@ -81,24 +78,15 @@ class WaremaShade(CoverEntity):
 
     @property
     def is_opening(self):
-        if self.is_moving and self.last_position > self.position:
-            return True
-        else:
-            return False
+        return self.is_moving and self.last_position > self.position
 
     @property
     def is_closing(self):
-        if self.is_moving and self.last_position < self.position:
-            return True
-        else:
-            return False
+        return self.is_moving and self.last_position < self.position
 
     @property
     def is_closed(self):
-        if not self.is_moving and self.position == 100:
-            return True
-        else:
-            return False
+        return not self.is_moving and self.position == 100
 
     def open_cover(self, **kwargs):
         self.force_update_until = datetime.now() + timedelta(seconds=15)
@@ -111,4 +99,3 @@ class WaremaShade(CoverEntity):
     def set_cover_position(self, **kwargs):
         self.force_update_until = datetime.now() + timedelta(seconds=15)
         self.shade.set_shade_position(100 - kwargs[ATTR_POSITION])
-
